d6b0b3504765dd1154735e6f958139f3d1f77f3a
cmatan10 <cmatan10@gmail.com>
2023-06-29 23:31:50 +0300

    MetaMaskSDK

--- src/index.js
+++ src/index.js
@@ -28,7 +28,24 @@ const App = () => {
  });
 
 
+ if (window.ethereum) {
+  handleEthereum();
+} else {
+  window.addEventListener('ethereum#initialized', handleEthereum, {
+    once: true,
+  });
+  setTimeout(handleEthereum, 3000); // 3 seconds
+}
 
+function handleEthereum() {
+  const { ethereum } = window;
+  if (ethereum && ethereum.isMetaMask) {
+    console.log('Ethereum successfully detected!');
+    // Access the decentralized web!
+  } else {
+    console.log('Please install MetaMask!');
+  }
+}
 
 
 
