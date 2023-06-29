db4b4600b13fd847de478f4011722984e81a5c5a
cmatan10 <cmatan10@gmail.com>
2023-06-30 00:43:57 +0300

    card style

--- src/index.js
+++ src/index.js
@@ -28,25 +28,6 @@ const App = () => {
  });
 
 
- if (window.ethereum) {
-  handleEthereum();
-} else {
-  window.addEventListener('ethereum#initialized', handleEthereum, {
-    once: true,
-  });
-  setTimeout(handleEthereum, 3000); // 3 seconds
-}
-
-function handleEthereum() {
-  const { ethereum } = window;
-  if (ethereum && ethereum.isMetaMask) {
-    console.log('Ethereum successfully detected!');
-    // Access the decentralized web!
-  } else {
-    console.log('Please install MetaMask!');
-  }
-}
-
 
 
 
