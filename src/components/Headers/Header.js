f650b38c6696aa67920acdcf4edf42bb1834dcc2
cmatan10 <cmatan10@gmail.com>
2023-06-30 17:44:17 +0300

    chain style

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -156,7 +156,7 @@ const Header = () => {
                       </div>
                       <span className="h2 font-weight-bold mb-0 " style={{ fontSize: '14px' }}>
                         {Chain
-                          ? <p className="d-flex align-items-center justify-content-center" style={{color:'#a3a4af'}}>
+                          ? <p className="d-flex align-items-center justify-content-center" style={{color:'white'}}>
                             {ChainToNetwork[Chain] || `Network 0x${Chain} is not supported`}
                           </p>
                           : <p className="h2 font-weight-bold mb-0" style={{ fontSize: '14px', color: 'white' }}>
